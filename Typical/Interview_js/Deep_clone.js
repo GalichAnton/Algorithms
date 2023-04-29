50199de111faac52efe9b3fd82bfd22a3be7e07b
anton_galich <inthesky133@gmail.com>
2023-04-29 08:58:41 +0300

    refactor deepClone

--- Typical/Interview_js/Deep_clone.js
+++ Typical/Interview_js/Deep_clone.js
@@ -21,3 +21,48 @@ const user2 = deepClone(user);
 console.log(user2);
 user.name = "sdfdsfsdf"
 console.log(user);
+
+const obj = {
+  a: 5,
+  b() {
+    return this
+  },
+  c: { d: [1,2,3],  e: 1 },
+  set: new Set([1,2,3]),
+  map : new Map([[1,2],['test','dfgd']]),
+  date: new Date(2023,04,28)
+};
+
+const deepClonePlus = (value) => {
+  if(Array.isArray(value)) {
+    return value.map(deepClonePlus)
+  }
+
+  if(typeof value === 'function') {
+    return value
+  }
+
+  if(value instanceof Map) {
+    return new Map(Array.from(value.entries(),deepClonePlus))
+  }
+
+  if(value instanceof Set) {
+    return new Set([...value].map(deepClonePlus))
+  }
+  
+  if(value instanceof Date) {
+    return new Date(value.getTime())
+  }
+
+  if(typeof value === 'object' && value !== null) {
+    const newObj = Object.create(Object.getPrototypeOf(value))
+    for(let key in value) {
+      newObj[key] = deepClonePlus(value[key])
+    }
+    return newObj
+  }
+
+  return value
+
+};
+console.log(deepClonePlus(obj))
